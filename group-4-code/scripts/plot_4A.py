eabbb2dd5859f622a8df6ede76c2cf5302666799
Alexandru Postu <A.N.Postu@student.tudelft.nl>
2025-06-15 10:05:54 +0300

    Latest version of plot 4A.

--- group-4-code/scripts/plot_4A.py
+++ group-4-code/scripts/plot_4A.py
@@ -8,8 +8,8 @@ def run_matrix_96():
         "matrix",
         "test_matrix_96_output",
         data,
-        minimum_signatures=5,
-        maximum_signatures=5,
+        minimum_signatures=4,
+        maximum_signatures=4,
         nmf_replicates=5,
         min_nmf_iterations=100,
         max_nmf_iterations=1000,
@@ -19,6 +19,7 @@ def run_matrix_96():
 
 if __name__ == "__main__":
     run_matrix_96()
-    src_path = "../../test_matrix_96_output/SBS96/All_Solutions/SBS96_5_Signatures/Signatures/Signature_plot/SBS_96_plots_S5.pdf"
-    dst_path = "../figures/figure_4A.pdf"
+    #src_path = "../test_matrix_96_output/SBS96/All_Solutions/SBS96_5_Signatures/Signatures/Signature_plot/SBS_96_plots_S5.pdf"
+    src_path = "../test_matrix_96_output/SBS96/All_Solutions/SBS96_4_Signatures/Signatures/Signature_plot/SBS_96_plots_S4.pdf"
+    dst_path = "../figures/figure_4A_S4.pdf"
     shutil.copyfile(src_path, dst_path)
